08ba1cd67046c1d72317fbde8e87bc2b43b94876
Iva <141308984+Iveto97@users.noreply.github.com>
2025-02-10 09:01:09 +0200

    Update goFight function
    Updating the location and monster's data

--- JavaScript Algorithms and Data Structures/role-game/script.js
+++ JavaScript Algorithms and Data Structures/role-game/script.js
@@ -142,7 +142,11 @@ function sellWeapon() {
 }
 
 function goFight() {
-  
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function fightSlime() {
