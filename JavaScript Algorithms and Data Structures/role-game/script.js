87b2583236899cdf78067275b77b5672b2dc55fd
Iva <141308984+Iveto97@users.noreply.github.com>
2025-02-11 08:20:49 +0200

    Add functionality of defeatMonster function

--- JavaScript Algorithms and Data Structures/role-game/script.js
+++ JavaScript Algorithms and Data Structures/role-game/script.js
@@ -65,7 +65,13 @@ const locations = [
         name: "fight",
         "button text": ["Attack", "Dodge", "Run"],
         "button functions": [attack, dodge, goTown],
-        text: "You are fighting a monster."
+        text: "You are fighting a monster.",
+      },
+      {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
       },
 ];
 
@@ -75,6 +81,8 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+    monsterStats.style.display = "none";
+    
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
@@ -179,11 +187,15 @@ function attack() {
 }
 
 function dodge() {
-  
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
 
 function defeatMonster () {
-
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
 }
 
 function lose() {
