d7559dd8b8e1a372b44f3f527b262babc2d24883
Iva <141308984+Iveto97@users.noreply.github.com>
2025-02-11 15:46:40 +0200

    Update script.js

--- JavaScript Algorithms and Data Structures/calorie-counter/script.js
+++ JavaScript Algorithms and Data Structures/calorie-counter/script.js
@@ -113,3 +113,4 @@ function clearForm() {
 
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener("click", clearForm);
