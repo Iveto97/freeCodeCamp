1cb27c55660da645f627bd74bd61e8604330befc
Iva <141308984+Iveto97@users.noreply.github.com>
2025-02-11 09:43:40 +0200

    Update script.js
    Give on monsters a dynamic attack value and life of weapons

--- JavaScript Algorithms and Data Structures/role-game/script.js
+++ JavaScript Algorithms and Data Structures/role-game/script.js
@@ -186,9 +186,16 @@ function fightDragon() {
 
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
-  text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-  health -= monsters[fighting].level;
-  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  if (isMonsterHit()) {
+    monsterHealth -=
+      weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  }  else {
+    text.innerText += " You miss.";
+  }
+
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
@@ -200,6 +207,19 @@ function attack() {
       defeatMonster();
     }
   }
+  if (Math.random() <= .1 && inventory.length !== 1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeaponIndex-- ;
+  }
+}
+
+function getMonsterAttackValue(level) {
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+  return Math.random() > .2 || health < 20;
 }
 
 function dodge() {
