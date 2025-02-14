a37aaef547ca8857948b3faed9ba3214804838c6
Iva <141308984+Iveto97@users.noreply.github.com>
2025-02-14 15:38:36 +0200

    Add function for playing the next song

--- JavaScript Algorithms and Data Structures/music-player/script.js
+++ JavaScript Algorithms and Data Structures/music-player/script.js
@@ -112,6 +112,19 @@ const pauseSong = () => {
 
 pauseButton.addEventListener("click", pauseSong);
 
+const playNextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+
+    playSong(nextSong.id);
+  }
+};
+
+nextButton.addEventListener("click", playNextSong);
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
