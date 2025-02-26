fa7fd6d2411e67da9c5f06ed6ff3e7cbaf92dd94
Iva <141308984+Iveto97@users.noreply.github.com>
2025-02-26 09:54:25 +0200

    Accessing properties
    Accessing properties from the myFavoriteFootballTeam object using object destructuring syntax.

--- JavaScript Algorithms and Data Structures/football-team-cards/script.js
+++ JavaScript Algorithms and Data Structures/football-team-cards/script.js
@@ -174,3 +174,7 @@ const myFavoriteFootballTeam = {
 
 Object.freeze(myFavoriteFootballTeam);
 
+const { sport, team, year, players } = myFavoriteFootballTeam;
+const { coachName } = myFavoriteFootballTeam.headCoach;
+
+
