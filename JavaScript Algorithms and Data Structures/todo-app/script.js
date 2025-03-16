c70be96b59a31e9fe00b7d32d677c292f09a8d35
Iva <141308984+Iveto97@users.noreply.github.com>
2025-03-16 13:37:33 +0200

    Save the task items to the local storage

--- JavaScript Algorithms and Data Structures/todo-app/script.js
+++ JavaScript Algorithms and Data Structures/todo-app/script.js
@@ -10,7 +10,7 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
 const addOrUpdateTask = () => {
@@ -27,6 +27,8 @@ const addOrUpdateTask = () => {
   } else {
     taskData[dataArrIndex] = taskObj;
   }
+
+  localStorage.setItem("data", JSON.stringify(taskData));
 
   updateTaskContainer();
   reset();
@@ -54,6 +56,7 @@ const deleteTask = (buttonEl) => {
   );
   buttonEl.parentElement.remove();
   taskData.splice(dataArrIndex, 1);
+  localStorage.setItem("data", JSON.stringify(taskData));
 };
 
 const editTask = (buttonEl) => {
@@ -79,6 +82,10 @@ const reset = () => {
   taskForm.classList.toggle("hidden");
   currentTask = {};
 };
+
+if(taskData.length) {
+  updateTaskContainer()
+}
 
 openTaskFormBtn.addEventListener("click", () => {
   taskForm.classList.toggle("hidden");
@@ -108,11 +115,3 @@ taskForm.addEventListener("submit", (e) => {
 
   addOrUpdateTask()
 });
-
-const myTaskArr = [
-  { task: "Walk the Dog", date: "22-04-2022" },
-  { task: "Read some books", date: "02-11-2023" },
-  { task: "Watch football", date: "10-08-2021" },
-];
-
-localStorage.setItem("data", JSON.stringify(myTaskArr));
