9716f44634de21896a9d3999bf49100a07b610ad
Iva <141308984+Iveto97@users.noreply.github.com>
2025-03-16 13:02:50 +0200

    Enable editing and deleting for each task

--- JavaScript Algorithms and Data Structures/todo-app/script.js
+++ JavaScript Algorithms and Data Structures/todo-app/script.js
@@ -24,26 +24,52 @@ const addOrUpdateTask = () => {
 
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
+  } else {
+    taskData[dataArrIndex] = taskObj;
   }
 
-  updateTaskContainer()
-  reset()
+  updateTaskContainer();
+  reset();
 };
 
 const updateTaskContainer = () => {
   tasksContainer.innerHTML = "";
-  
+
   taskData.forEach(({ id, title, date, description }) => {
     tasksContainer.innerHTML += `
         <div class="task" id="${id}">
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button>
+          <button type="button" class="btn" onclick="editTask(this)>Edit</button>
+          <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
         </div>
       `;
   });
+};
+
+const deleteTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+};
+
+const editTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+
+  taskForm.classList.toggle("hidden");
 };
 
 const reset = () => {
