09cf17bf527180b9d947fddc42747d9a35f9490e
Iva <141308984+Iveto97@users.noreply.github.com>
2025-03-18 17:29:37 +0200

    Add README file

---
+++ JavaScript Algorithms and Data Structures/todo-app/README.md
@@ -0,0 +1,51 @@
+# TODO App
+
+### **Project Description**
+
+With this web application, you can manage and track your tasks. You can create new tasks with a custom title, date and additional description.
+When a task is completed you can delete it.
+
+### **Installation**
+You need the latest browser to be able to View.
+Follow this link:
+
+   https://github.com/Iveto97/freeCodeCamp/tree/main/JavaScript%20Algorithms%20and%20Data%20Structures/todo-app
+
+It is hosted by gitHub.
+
+To access this project on your local files, you can download it using these steps:
+1. Open your browser
+2. Paste this link 
+   
+   https://downgit.github.io/#/home?url=https://github.com/Iveto97/freeCodeCamp/tree/main/JavaScript%20Algorithms%20and%20Data%20Structures/todo-app
+
+3. This will save .zip file into your download folder
+4. Extract files
+5. Open the folder
+6. Run `index.html` with an active internet
+
+### **Usage**
+   The application, displays user's tasks with title. date and description. Users can create tasks and delete them when they are completed.
+
+The application supports the following options:
+
+- Add new task: Create new task with title, date and description
+- Update task: Change the details (title, date or description) of the created task
+- Delete task: Delete completed task
+  
+
+### **Technologies**
+1. HTML
+2. CSS
+3. JavaScript
+4. Git
+
+### **License**
+
+MIT licensed
+
+### **Screenshots**
+The Application
+
+<img src="todo.PNG" alt="All tasks" width="400"  height="300"/>
+<img src="new-task.PNG" alt="Add new task" width="400"  height="300"/>
--- JavaScript Algorithms and Data Structures/todo-app/script.js
+++ JavaScript Algorithms and Data Structures/todo-app/script.js
@@ -11,24 +11,23 @@ const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
 const taskData = JSON.parse(localStorage.getItem("data")) || [];
-
-const removeSpecialChars = (string) => {
-  return string.replace(/[^a-zA-Z0-9 ]/g, "");
-};
-
 let currentTask = {};
 
+const removeSpecialChars = (val) => {
+  return val.trim().replace(/[^A-Za-z0-9\-\s]/g, '')
+}
+
 const addOrUpdateTask = () => {
-  if(!titleInput.value.trim()) {
+   if(!titleInput.value.trim()){
     alert("Please provide a title");
     return;
   }
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
-    id: `${removeSpecialChars(titleInput.value).toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    title: removeSpecialChars(titleInput.value),
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
     date: dateInput.value,
-    description: removeSpecialChars(descriptionInput.value),
+    description: descriptionInput.value,
   };
 
   if (dataArrIndex === -1) {
@@ -38,38 +37,41 @@ const addOrUpdateTask = () => {
   }
 
   localStorage.setItem("data", JSON.stringify(taskData));
-
-  updateTaskContainer();
-  reset();
+  updateTaskContainer()
+  reset()
 };
 
 const updateTaskContainer = () => {
   tasksContainer.innerHTML = "";
 
-  taskData.forEach(({ id, title, date, description }) => {
-    tasksContainer.innerHTML += `
+  taskData.forEach(
+    ({ id, title, date, description }) => {
+        (tasksContainer.innerHTML += `
         <div class="task" id="${id}">
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn" onclick="editTask(this)>Edit</button>
-          <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
-      `;
-  });
+      `)
+    }
+  );
 };
+
 
 const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+
   buttonEl.parentElement.remove();
   taskData.splice(dataArrIndex, 1);
   localStorage.setItem("data", JSON.stringify(taskData));
-};
+}
 
 const editTask = (buttonEl) => {
-  const dataArrIndex = taskData.findIndex(
+    const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
 
@@ -81,8 +83,8 @@ const editTask = (buttonEl) => {
 
   addOrUpdateTaskBtn.innerText = "Update Task";
 
-  taskForm.classList.toggle("hidden");
-};
+  taskForm.classList.toggle("hidden");  
+}
 
 const reset = () => {
   addOrUpdateTaskBtn.innerText = "Add Task";
@@ -91,22 +93,21 @@ const reset = () => {
   descriptionInput.value = "";
   taskForm.classList.toggle("hidden");
   currentTask = {};
-};
-
-if(taskData.length) {
-  updateTaskContainer()
 }
 
-openTaskFormBtn.addEventListener("click", () => {
-  taskForm.classList.toggle("hidden");
-});
+if (taskData.length) {
+  updateTaskContainer();
+}
+
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
 
 closeTaskFormBtn.addEventListener("click", () => {
-  const formInputsContainValues =
-    titleInput.value || dateInput.value || descriptionInput.value;
-    const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description
+  const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
 
-  if (formInputsContainValues) {
+  if (formInputsContainValues && formInputValuesUpdated) {
     confirmCloseDialog.showModal();
   } else {
     reset();
@@ -117,11 +118,11 @@ cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
 discardBtn.addEventListener("click", () => {
   confirmCloseDialog.close();
-  reset();
+  reset()
 });
 
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  addOrUpdateTask()
+  addOrUpdateTask();
 });
