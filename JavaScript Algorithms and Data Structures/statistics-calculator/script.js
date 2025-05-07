5137bfed8bc17896ba84bb056a4754d2183837cc
Iva <141308984+Iveto97@users.noreply.github.com>
2025-05-07 10:06:43 +0300

    Update script.js

--- JavaScript Algorithms and Data Structures/statistics-calculator/script.js
+++ JavaScript Algorithms and Data Structures/statistics-calculator/script.js
@@ -1,18 +1,6 @@
 const getMean = (array) =>
   array.reduce((acc, el) => acc + el, 0) / array.length;
 
-const calculate = () => {
-  const value = document.querySelector("#numbers").value;
-  const array = value.split(/,\s*/g);
-  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
-
-  const mean = getMean(numbers);
-  const median = getMedian(numbers);
-
-  document.querySelector("#mean").textContent = mean;
-  document.querySelector("#median").textContent = median;
-};
-
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
   const median =
@@ -20,4 +8,33 @@ const getMedian = (array) => {
       ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
       : sorted[Math.floor(sorted.length / 2)];
   return median;
+};
+
+const getMode = (array) => {
+  const counts = {};
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  });
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
+};
+
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+
+  const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  const mode = getMode(numbers);
+
+  document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
 };
