7b7bf6363c08b47af1f8735321e1cbb5596bed20
Iva <141308984+Iveto97@users.noreply.github.com>
2025-05-23 10:11:40 +0300

    Update script.js

--- JavaScript Algorithms and Data Structures/spreadsheet/script.js
+++ JavaScript Algorithms and Data Structures/spreadsheet/script.js
@@ -1,6 +1,18 @@
-const isEven = num => num % 2 === 0;
+const infixToFunction = {
+  "+": (x, y) => x + y,
+  "-": (x, y) => x - y,
+  "*": (x, y) => x * y,
+  "/": (x, y) => x / y,
+};
+
+const infixEval = (str, regex) =>
+  str.replace(regex, (_match, arg1, operator, arg2) =>
+    infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
+  );
+
+const isEven = (num) => num % 2 === 0;
 const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
-const average = nums => sum(nums) / nums.length;
+const average = (nums) => sum(nums) / nums.length;
 
 const median = (nums) => {
   const sorted = nums.slice().sort((a, b) => a - b);
@@ -27,7 +39,6 @@ const charRange = (start, end) =>
     String.fromCharCode(code)
   );
 
-  
 const evalFormula = (x, cells) => {
   const idToText = (id) => cells.find((cell) => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
