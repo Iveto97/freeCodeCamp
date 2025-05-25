93d0196180ebfcf3fd1a93b64389e75d00ba5cc7
Iva <141308984+Iveto97@users.noreply.github.com>
2025-05-25 09:43:57 +0300

    Update script.js

--- JavaScript Algorithms and Data Structures/spreadsheet/script.js
+++ JavaScript Algorithms and Data Structures/spreadsheet/script.js
@@ -10,6 +10,12 @@ const infixEval = (str, regex) =>
     infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
   );
 
+const highPrecedence = (str) => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+  return str === str2 ? str : highPrecedence(str2);
+};
+
 const isEven = (num) => num % 2 === 0;
 const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
 const average = (nums) => sum(nums) / nums.length;
