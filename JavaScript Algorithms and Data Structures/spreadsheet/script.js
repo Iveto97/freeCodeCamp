6d14dcb40fb3e6aacdff05328f37e0e2f6a692c8
Iva <141308984+Iveto97@users.noreply.github.com>
2025-06-02 10:49:01 +0300

    Update script.js

--- JavaScript Algorithms and Data Structures/spreadsheet/script.js
+++ JavaScript Algorithms and Data Structures/spreadsheet/script.js
@@ -76,6 +76,10 @@ const evalFormula = (x, cells) => {
   const cellExpanded = rangeExpanded.replace(cellRegex, (match) =>
     idToText(match.toUpperCase())
   );
+  const functionExpanded = applyFunction(cellExpanded);
+  return functionExpanded === x
+    ? functionExpanded
+    : evalFormula(functionExpanded, cells);
 };
 
 window.onload = () => {
