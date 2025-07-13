8353ef86fca83227e76e6be1b96958bb98ca27f9
Iva <141308984+Iveto97@users.noreply.github.com>
2025-07-13 10:08:13 +0300

    Add clearCart function
    Add a function that will clear the customer's cart

--- JavaScript Algorithms and Data Structures/shopping-card/script.js
+++ JavaScript Algorithms and Data Structures/shopping-card/script.js
@@ -86,7 +86,6 @@ const products = [
   },
 ];
 
-
 products.forEach(({ name, id, price, category }) => {
   dessertCards.innerHTML += `
       <div class="dessert-card">
@@ -139,6 +138,27 @@ class ShoppingCart {
     return this.items.length;
   }
 
+  clearCart() {
+    if (!this.items.length) {
+      alert("Your shopping cart is already empty");
+      return;
+    }
+
+    const isCartCleared = confirm(
+      "Are you sure you want to clear all items from your shopping cart?"
+    );
+
+    if (isCartCleared) {
+      this.items = [];
+      this.total = 0;
+      productsContainer.innerHTML = "";
+      totalNumberOfItems.textContent = 0;
+      cartSubTotal.textContent = 0;
+      cartTaxes.textContent = 0;
+      cartTotal.textContent = 0;
+    }
+  }
+
   calculateTaxes(amount) {
     return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
   }
@@ -152,7 +172,7 @@ class ShoppingCart {
     cartTotal.textContent = `$${this.total.toFixed(2)}`;
     return this.total;
   }
-};
+}
 
 const cart = new ShoppingCart();
 const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
@@ -170,3 +190,5 @@ cartBtn.addEventListener("click", () => {
   showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
   cartContainer.style.display = isCartShowing ? "block" : "none";
 });
+
+clearCartBtn.addEventListener("click", cart.clearCart.bind(cart));
