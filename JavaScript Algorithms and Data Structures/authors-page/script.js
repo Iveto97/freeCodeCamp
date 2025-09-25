c6f822f3ad3fe8a949b4e8397ad31e4b28b3e299
Iva <141308984+Iveto97@users.noreply.github.com>
2025-09-25 10:44:42 +0300

    Call the displayAuthors function

--- JavaScript Algorithms and Data Structures/authors-page/script.js
+++ JavaScript Algorithms and Data Structures/authors-page/script.js
@@ -4,7 +4,8 @@ const loadMoreBtn = document.getElementById("load-more-btn");
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    authorDataArr = data;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
   })
   .catch((err) => {
     console.error(`There was an error: ${err}`);
