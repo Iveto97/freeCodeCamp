8a9231db30370177887922db0ad7f84fbaa2f200
Iva <iveto9244@gmail.com>
2026-07-30 08:36:20 +0300

    Modified team cards

--- JavaScript Algorithms and Data Structures/football-team-cards/script.js
+++ JavaScript Algorithms and Data Structures/football-team-cards/script.js
@@ -1,227 +1,177 @@
-const teamName = document.getElementById("team");
-const typeOfSport = document.getElementById("sport");
-const worldCupYear = document.getElementById("year");
-const headCoach = document.getElementById("head-coach");
-const playerCards = document.getElementById("player-cards");
-const playersDropdownList = document.getElementById("players");
-
-const myFavoriteFootballTeam = {
-  team: "Argentina",
-  sport: "Football",
-  year: 1986,
-  isWorldCupWinner: true,
-  headCoach: {
-    coachName: "Carlos Bilardo",
-    matches: 7,
-  },
+const footballTeam = {
+  team: "Real Madrid",
+  year: 1902,
+  headCoach: "Jose Mourinho",
   players: [
     {
-      name: "Sergio Almirón",
-      position: "forward",
+      name: "Thibaut Courtois",
+      position: "goalkeeper",
       number: 1,
       isCaptain: false,
-      nickname: null,
     },
     {
-      name: "Sergio Batista",
-      position: "midfielder",
-      number: 2,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "Ricardo Bochini",
-      position: "midfielder",
+      name: "Éder Militão",
+      position: "defender",
       number: 3,
       isCaptain: false,
-      nickname: "El Bocha",
     },
     {
-      name: "Claudio Borghi",
+      name: "Jude Bellingham",
       position: "midfielder",
-      number: 4,
-      isCaptain: false,
-      nickname: "Bichi",
-    },
-    {
-      name: "José Luis Brown",
-      position: "defender",
       number: 5,
       isCaptain: false,
-      nickname: "Tata",
     },
     {
-      name: "Daniel Passarella",
-      position: "defender",
+      name: "Eduardo Camavinga",
+      position: "midfielder",
       number: 6,
       isCaptain: false,
-      nickname: "El Gran Capitán",
     },
     {
-      name: "Jorge Burruchaga",
+      name: "Vinícius Júnior",
       position: "forward",
       number: 7,
       isCaptain: false,
-      nickname: "Burru",
     },
     {
-      name: "Néstor Clausen",
-      position: "defender",
-      number: 8,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "José Luis Cuciuffo",
-      position: "defender",
-      number: 9,
-      isCaptain: false,
-      nickname: "El Cuchu",
-    },
-    {
-      name: "Diego Maradona",
+      name: "Federico Valverde",
       position: "midfielder",
-      number: 10,
+      number: 8,
       isCaptain: true,
-      nickname: "El Pibe de Oro",
     },
     {
-      name: "Jorge Valdano",
+      name: "Kylian Mbappé",
+      position: "forward",
+      number: 10,
+      isCaptain: false,
+    },
+    {
+      name: "Rodrygo",
       position: "forward",
       number: 11,
       isCaptain: false,
-      nickname: "The Philosopher of Football",
     },
     {
-      name: "Héctor Enrique",
-      position: "midfielder",
+      name: "Trent Alexander-Arnold",
+      position: "defender",
       number: 12,
       isCaptain: false,
-      nickname: null,
     },
     {
-      name: "Oscar Garré",
-      position: "defender",
+      name: "Andriy Lunin",
+      position: "goalkeeper",
       number: 13,
       isCaptain: false,
-      nickname: null,
     },
     {
-      name: "Ricardo Giusti",
+      name: "Aurélien Tchouaméni",
       position: "midfielder",
       number: 14,
       isCaptain: false,
-      nickname: null,
     },
     {
-      name: "Luis Islas",
-      position: "goalkeeper",
+      name: "Arda Güler",
+      position: "midfielder",
       number: 15,
       isCaptain: false,
-      nickname: "El loco",
     },
     {
-      name: "Julio Olarticoechea",
-      position: "defender",
+      name: "Gonzalo García",
+      position: "forward",
       number: 16,
       isCaptain: false,
-      nickname: null,
     },
     {
-      name: "Pedro Pasculli",
-      position: "forward",
+      name: "Raúl Asencio",
+      position: "defender",
       number: 17,
       isCaptain: false,
-      nickname: null,
     },
     {
-      name: "Nery Pumpido",
-      position: "goalkeeper",
+      name: "Álvaro Carreras",
+      position: "defender",
       number: 18,
       isCaptain: false,
-      nickname: null,
     },
     {
-      name: "Oscar Ruggeri",
-      position: "defender",
-      number: 19,
-      isCaptain: false,
-      nickname: "El Cabezón",
-    },
-    {
-      name: "Carlos Tapia",
-      position: "midfielder",
-      number: 20,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "Marcelo Trobbiani",
-      position: "midfielder",
+      name: "Brahim Díaz",
+      position: "forward",
       number: 21,
       isCaptain: false,
-      nickname: "Calesita",
     },
     {
-      name: "Héctor Zelada",
-      position: "goalkeeper",
+      name: "Antonio Rüdiger",
+      position: "defender",
       number: 22,
       isCaptain: false,
-      nickname: null,
+    },
+    {
+      name: "Ferland Mendy",
+      position: "defender",
+      number: 23,
+      isCaptain: false,
+    },
+    {
+      name: "Dean Huijsen",
+      position: "defender",
+      number: 24,
+      isCaptain: false,
+    },
+    {
+      name: "Franco Mastantuono",
+      position: "forward",
+      number: 30,
+      isCaptain: false,
     },
   ],
 };
 
-Object.freeze(myFavoriteFootballTeam);
+const teamName = document.getElementById("team");
+const year = document.getElementById("year");
+const headCoach = document.getElementById("head-coach");
+const playerCard = document.getElementById("player-cards");
+const selectPlayerPosition = document.getElementById("players");
 
-const { sport, team, year, players } = myFavoriteFootballTeam;
-const { coachName } = myFavoriteFootballTeam.headCoach;
+function playerCardCreator(playerData) {
 
-typeOfSport.textContent = sport;
-teamName.textContent = team;
-worldCupYear.textContent = year;
-headCoach.textContent = coachName;
+  return `
+    <div class="player-card">
+      <img src="./Real Madrid.png" alt="Real Madrid" class="team-logo">
+      <h2>${playerData.isCaptain ? "(Captain)" : ""} ${playerData.name}</h2>
+      <p>Position: ${playerData.position}</p>
+      <p>Number: ${playerData.number}</p>
+    </div>
+  `;
+}
 
-const setPlayerCards = (arr = players) => {
-  playerCards.innerHTML += arr
-    .map(({ name, position, number, isCaptain, nickname }) => {
-      return `
-        <div class="player-card">
-          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
-          <p>Position: ${position}</p>
-          <p>Number: ${number}</p>
-          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
-        </div>
-    `;
-    })
+function renderTeamInfo() {
+  teamName.textContent = footballTeam.team;
+  year.textContent = footballTeam.year;
+  headCoach.textContent = footballTeam.headCoach;
+}
+
+function renderPlayerCards(playersData) {
+  const playerCardsHTML = playersData
+    .map((player) => playerCardCreator(player))
     .join("");
-};
+  playerCard.innerHTML = playerCardsHTML;
+}
 
-playersDropdownList.addEventListener("change", (e) => {
-  playerCards.innerHTML = "";
-  switch (e.target.value) {
-    case "nickname":
-      setPlayerCards(players.filter((player) => player.nickname !== null));
-      break;
-    case "forward":
-      setPlayerCards(players.filter((player) => player.position === "forward"));
-      break;
-    case "midfielder":
-      setPlayerCards(
-        players.filter((player) => player.position === "midfielder")
-      );
-      break;
-    case "defender":
-      setPlayerCards(
-        players.filter((player) => player.position === "defender")
-      );
-      break;
-    case "goalkeeper":
-      setPlayerCards(
-        players.filter((player) => player.position === "goalkeeper")
-      );
-      break;
-    default:
-      setPlayerCards();
-  }
+function filterPlayersByPosition(position) {
+  const data = footballTeam.players;
+  const filteredPlayers =
+    position === "all"
+      ? data
+      : data.filter((player) => player.position === position);
+  renderPlayerCards(filteredPlayers);
+  console.log(filteredPlayers);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderTeamInfo();
+  renderPlayerCards(footballTeam.players);
 });
+
+selectPlayerPosition.addEventListener("change", (event) =>
+  filterPlayersByPosition(event.target.value),
+);
